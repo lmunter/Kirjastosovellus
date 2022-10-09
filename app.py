65b4f564dbf046fb3e6b01233e8f69099f8c7020
lmunter <90262765+lmunter@users.noreply.github.com>
2022-10-09 17:44:46 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from os import getenv
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("SOVELLUS_DB_URL")
+print(app.config)
 db = SQLAlchemy(app)
 app.secret_key = getenv("SECRET_KEY")
 
